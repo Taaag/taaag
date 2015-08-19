40d2fb8217d24a0e69e3f5fe75fc61607087e5b1
liuyang0142 <melody9951213@gmail.com>
2015-08-19 22:25:49 +0800

    add tag ang tagging models

--- app/models.py
+++ app/models.py
@@ -2,14 +2,41 @@ from datetime import datetime
 
 from app import db
 
+from sqlalchemy.dialects.mysql import BIGINT, SMALLINT
+
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.String, nullable=False, primary_key=True)
+    id = db.Column(BIGINT(unsigned=True), nullable=False, primary_key=True)
     created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
                         onupdate=datetime.utcnow)
     name = db.Column(db.String, nullable=False)
     profile_url = db.Column(db.String, nullable=False)
     access_token = db.Column(db.String, nullable=False)
+    privacy = db.Column(SMALLINT, default=0, nullable=False)
+
+
+class Tag(db.Model):
+    __tablename__ = 'tags'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
+                        onupdate=datetime.utcnow)
+
+    name = db.Column(db.String, nullable=False)
+
+
+class Tagging(db.Model):
+    __tablename__ = 'taggings'
+
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
+                        onupdate=datetime.utcnow)
+
+    tagger_uid = db.Column(BIGINT(unsigned=True), db.ForeignKey('users.id'), nullable=False)
+    taggee_uid = db.Column(BIGINT(unsigned=True), db.ForeignKey('users.id'), nullable=False)
+    tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), nullable=False)
