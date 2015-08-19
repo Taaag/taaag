bf65021e95d78e50ffbf1fe951c5c9f9efe2d1ff
liuyang0142 <melody9951213@gmail.com>
2015-08-20 03:20:51 +0800

    make [tagger, taggee, tag] uniq, some basic query

--- app/models.py
+++ app/models.py
@@ -1,25 +1,35 @@
 from datetime import datetime
 
 from app import db
-
-from sqlalchemy.dialects.mysql import BIGINT, SMALLINT
+from sqlalchemy import func
+from sqlalchemy.orm import aliased
 
 
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(BIGINT(unsigned=True), nullable=False, primary_key=True)
+    id = db.Column(db.Integer, nullable=False, primary_key=True)
     created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
                         onupdate=datetime.utcnow)
     name = db.Column(db.String, nullable=False)
     profile_url = db.Column(db.String, nullable=False)
     access_token = db.Column(db.String, nullable=False)
-    privacy = db.Column(SMALLINT, default=0, nullable=False)
+    privacy = db.Column(db.Integer, default=0, nullable=False)
 
     # Relationships
     tags = db.relationship('Tagging', backref='taggee', foreign_keys='Tagging.taggee_id', lazy='dynamic')
     tag_others = db.relationship('Tagging', backref='tagger', foreign_keys='Tagging.tagger_id', lazy='dynamic')
+
+    def get_settings(self):
+        return self.privacy
+
+    def get_tags(self):
+        return self.tags.join(Tag).with_entities(Tag.name, func.count()).group_by(Tag.name).all()
+
+    def get_tags_with_tagger(self):
+        tagger = aliased(User, name="tagger")
+        return self.tags.join(Tag).join(tagger, tagger.id == Tagging.tagger_id).with_entities(Tag.name, tagger).all()
 
 
 class Tag(db.Model):
@@ -53,6 +63,8 @@ class Tagging(db.Model):
     created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
                         onupdate=datetime.utcnow)
-    tagger_id = db.Column(BIGINT(unsigned=True), db.ForeignKey('users.id'), nullable=False)
-    taggee_id = db.Column(BIGINT(unsigned=True), db.ForeignKey('users.id'), nullable=False)
+    tagger_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    taggee_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     tag_id = db.Column(db.Integer, db.ForeignKey('tags.id'), nullable=False)
+
+    uniq = db.Index([tagger_id, taggee_id, tag_id], unique=True)
