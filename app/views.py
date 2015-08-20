4ffd65642fe8804690806f5d49279a905fa22a7a
CNA-Bld <cna.bld@gmail.com>
2015-08-20 18:42:36 +0800

    no message

--- app/views.py
+++ app/views.py
@@ -18,8 +18,8 @@ def index():
     # the user is logged in.
     if g.user:
         return render_template('index.html', app_id=FB_APP_ID,
-                               app_name=FB_APP_NAME, user=g.user,
-                               friends=get_user_friends(g.user['access_token']))
+                               app_name=FB_APP_NAME, user_name=g.user,
+                               friends=get_user_friends(g.user.access_token))
     # Otherwise, a user is not logged in.
     return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
 
@@ -62,50 +62,21 @@ def test():
 
 @app.before_request
 def get_current_user():
-    """Set g.user to the currently logged in user.
+    if not session.get('user'):
+        result = get_user_from_cookie(cookies=request.cookies, app_id=FB_APP_ID,
+                                      app_secret=FB_APP_SECRET)
+        if result:
+            user = User.get_by_id(result['uid'])
+            if not user:
+                graph = GraphAPI(result['access_token'])
+                profile = graph.get_object('me', fields='link,name,id')
+                access_token = graph.extend_access_token(FB_APP_ID, FB_APP_SECRET)
 
-    Called before each request, get_current_user sets the global g.user
-    variable to the currently logged in user.  A currently logged in user is
-    determined by seeing if it exists in Flask's session dictionary.
+                new_user = User(id=profile['id'], name=profile['name'],
+                            profile_url=profile['link'],
+                            access_token=access_token['access_token'])
+                new_user.insert()
+            session['user'] = user.id
 
-    If it is the first time the user is logging into this application it will
-    create the user and insert it into the database.  If the user is not logged
-    in, None will be set to g.user.
-    """
-
-    # Set the user in the session dictionary as a global g.user and bail out
-    # of this function early.
-    if session.get('user'):
-        g.user = session.get('user')
-        return
-
-    # Attempt to get the short term access token for the current user.
-    result = get_user_from_cookie(cookies=request.cookies, app_id=FB_APP_ID,
-                                  app_secret=FB_APP_SECRET)
-
-    # If there is no result, we assume the user is not logged in.
-    if result:
-        # Check to see if this user is already in our database.
-        user = User.query.filter(User.id == result['uid']).first()
-
-        if not user:
-            # Not an existing user so get info
-            graph = GraphAPI(result['access_token'])
-            profile = graph.get_object('me', fields='link,name,id')
-
-            # Create the user and insert it into the database
-            user = User(id=profile['id'], name=profile['name'],
-                        profile_url=profile['link'],
-                        access_token=result['access_token'])
-            db.session.add(user)
-        elif user.access_token != result['access_token']:
-            # If an existing user, update the access token
-            user.access_token = result['access_token']
-
-        # Add the user to the current session
-        session['user'] = dict(name=user.name,
-                               id=user.id, access_token=user.access_token)
-
-    # Commit changes to the database and set the user as a global g.user
-    db.session.commit()
-    g.user = session.get('user', None)
+    g.uid = session.get('user', None)
+    g.user = User.get_by_id(g.uid) if g.uid else None
