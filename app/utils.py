22792ec11b396a6183d26210c038edbdaf8b1564
CNA-Bld <cna.bld@gmail.com>
2015-08-21 16:13:32 +0800

    implement get user friends without cache

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,12 @@
 import facebook
+import requests
 
 def get_user_friends(access_token):
     graph = facebook.GraphAPI(access_token)
     user = graph.get_object("me")
-    return graph.get_connections(user["id"], "friends")
+    data = graph.get_connections(user["id"], "friends")
+    combined_data = data['data']
+    while 'next' in data['paging']:
+        data = requests.get(data['paging']['next']).json()
+        combined_data.extend(data['data'])
+    return combined_data
