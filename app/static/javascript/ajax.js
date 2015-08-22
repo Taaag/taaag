ba9b0f30dd9578b26845b5e241f1127167aa83af
Liu Yang <liuyang@r-239-99-25-172.comp.nus.edu.sg>
2015-08-22 15:42:48 +0800

    sdfad

--- app/static/javascript/ajax.js
+++ app/static/javascript/ajax.js
@@ -5,25 +5,27 @@
 
   $(document).ready(function() {
     console.log('wtf');
+
+    $('button').click(function() {
+        console.log('wtf');
+    });
+
+    $('#my-tags').click(function() {
+        console.log('llllll');
+        // $.ajax({
+        //   type: 'GET',
+        //   url: myTagUrl,
+        //   data: {
+        //     target: "user",
+        //     method: "my_tags"
+        //   },
+        //   success: function(response) {
+        //     console.log(response);
+        //   }
+        // });
+    });
   });
 
-  $('button').click(function() {
-      console.log('wtf');
-  });
 
-  $('#my-tags').click(function() {
-      console.log('llllll');
-      // $.ajax({
-      //   type: 'GET',
-      //   url: myTagUrl,
-      //   data: {
-      //     target: "user",
-      //     method: "my_tags"
-      //   },
-      //   success: function(response) {
-      //     console.log(response);
-      //   }
-      // });
-  });
 
 }(jQuery));
