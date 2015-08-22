5dae1df0d452ca821a0aafa4486b5c33562c3ed9
Liu Yang <liuyang@r-239-99-25-172.comp.nus.edu.sg>
2015-08-22 15:33:51 +0800

    ddd

--- app/static/javascript/ajax.js
+++ app/static/javascript/ajax.js
@@ -1,20 +1,25 @@
 (function($) {
   'use strict';
 
-  var myTagUrl = "https://taaag.sshz.org/api";
+  var myTagUrl = "http://taaag.sshz.org/api";
+
+  $(document).ready(function() {
+    console.log('wtf');
+  });
 
   $('#my-tags').click(function() {
-      $.ajax({
-        type: 'GET',
-        url: myTagUrl,
-        data: {
-          target: "user",
-          method: "my_tags"
-        },
-        success: function(response) {
-          console.log(response);
-        }
-      });
+      console.log('llllll');
+      // $.ajax({
+      //   type: 'GET',
+      //   url: myTagUrl,
+      //   data: {
+      //     target: "user",
+      //     method: "my_tags"
+      //   },
+      //   success: function(response) {
+      //     console.log(response);
+      //   }
+      // });
   });
 
 }(jQuery));
