709cf7671f2eec64adc007d02320be636eed487e
Liu Yang <liuyang@r-239-99-25-172.comp.nus.edu.sg>
2015-08-22 15:53:36 +0800

    try

--- app/static/javascript/ajax.js
+++ app/static/javascript/ajax.js
@@ -4,12 +4,6 @@
   var myTagUrl = "/api";
 
   $(document).ready(function() {
-    console.log('wtf');
-
-    $('button').click(function() {
-        console.log('wtf');
-    });
-
     $('#my-tags').click(function() {
         $.get(myTagUrl,
           {
