5058ad86f91ccb64747eceee2954b31a64d60cf1
Liu Yang <liuyang@r-19-103-25-172.comp.nus.edu.sg>
2015-08-24 17:59:34 +0800

    change login view

--- app/static/javascript/change_view.js
+++ app/static/javascript/change_view.js
@@ -1,9 +1,6 @@
 (function ($) {
     'use strict';
 
-    var VIEW_URL = "/api/";
-    var API_URL = "/api/";
-
     function loadView(view, data) {
         $.get('/change_view/' + view, data, function(response) {
             $('#content-view').html(response);
