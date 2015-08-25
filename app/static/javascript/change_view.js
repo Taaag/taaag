4d90cd86c36095dbe891960735ceb5b31a50f72e
CNA-Bld <cna.bld@gmail.com>
2015-08-25 16:05:47 +0800

    add spinner when loading

--- app/static/javascript/change_view.js
+++ app/static/javascript/change_view.js
@@ -1,7 +1,10 @@
 (function ($) {
     'use strict';
 
+    var loading = '<div class="inner-circles-loader">Loading…</div>';
+
     function loadView(view, data) {
+        $('#content-view').html(loading);
         $.get('/change_view/' + view, data, function (response) {
             $('#content-view').html(response);
         });
