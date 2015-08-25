1b87f29851a05b56552facf3e82de56aa7e034c0
CNA-Bld <cna.bld@gmail.com>
2015-08-26 00:23:05 +0800

    change spinner

--- app/static/javascript/change_view.js
+++ app/static/javascript/change_view.js
@@ -1,7 +1,7 @@
 (function ($) {
     'use strict';
 
-    var loading = '<div class="inner-circles-loader">Loading…</div>';
+    var loading = '<div class="spinner"><div class="bounce1"></div><div class="bounce2"></div><div class="bounce3"></div></div>';
 
     var historyStack = [];
     var currentView;
