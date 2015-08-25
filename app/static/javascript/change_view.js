8b49dd78c81cbe644566411692e3df49cb2f48b7
CNA-Bld <cna.bld@gmail.com>
2015-08-25 21:01:39 +0800

    add back button

--- app/static/javascript/change_view.js
+++ app/static/javascript/change_view.js
@@ -3,7 +3,10 @@
 
     var loading = '<div class="inner-circles-loader">Loading…</div>';
 
+    var historyStack = [];
+
     function loadView(view, data) {
+        historyStack.push([view, data]);
         $('#content-view').html(loading);
         $.get('/change_view/' + view, data, function (response) {
             $('#content-view').html(response);
@@ -13,6 +16,11 @@
 
     $(document).on("viewChanging", function (event, view, data) {
         loadView(view, data);
+    });
+
+    $('#back-btn').click(function () {
+        var data = historyStack.pop();
+        loadView(data[0], data[1]);
     });
 
     $(document).ready(function () {
