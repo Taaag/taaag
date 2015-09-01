b8c4c1035f071b4ff2fa35a8327eeeca63e5d01f
liuyang <melody9951213@gmail.com>
2015-09-01 15:01:03 +0800

    坏了别打我

--- app/models.py
+++ app/models.py
@@ -185,7 +185,7 @@ class Tagging(db.Model):
     __tablename__ = 'taggings'
 
     id = db.Column(db.Integer, nullable=False, primary_key=True)
-    created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    created = db.Column(db.DateTime, default=datetime.utcnow().replace(tzinfo=timezone.utc), nullable=False)
     updated = db.Column(db.DateTime, default=datetime.utcnow, nullable=False,
                         onupdate=datetime.utcnow)
     tagger_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='cascade'), nullable=False)
