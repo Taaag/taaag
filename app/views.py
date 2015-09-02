56badacf05d1da529929501460fffe7088b7f1dc
CNA-Bld <cna.bld@gmail.com>
2015-09-02 14:34:31 +0800

    跑的比那些个西方记者都快

--- app/views.py
+++ app/views.py
@@ -20,9 +20,7 @@ FB_APP_SECRET = '***REMOVED***'
 def index():
     # If a user was set in the get_current_user function before the request,
     # the user is logged in.
-    if g.user:
-        if not g.user.friends_api_authorized():
-            return '卧槽 You have not authorized us!'
+    if g.user and g.user.friends_api_authorized():
         return render_template('base.html', app_id=FB_APP_ID, app_name=FB_APP_NAME)
     # Otherwise, a user is not logged in.
     return render_template('login.html', app_id=FB_APP_ID, name=FB_APP_NAME)
