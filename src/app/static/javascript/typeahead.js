ed07ac0685df1426f36cdfbd535d07aca29ff759
CNA-Bld <cna.bld@gmail.com>
2015-09-04 20:22:56 +0800

    test invitable friends in search

--- src/app/static/javascript/typeahead.js
+++ src/app/static/javascript/typeahead.js
@@ -26,8 +26,26 @@
             }
         });
 
+        var invitable_friends = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            prefetch: {
+                url: '/api/invitable_friends',
+                cache: false,
+                transform: function (response) {
+                    return response['response']
+                }
+            }
+        });
+
         $('#search-bar').typeahead({highlight: true, hint: false},
             {
+                name: 'tags',
+                source: tags,
+                templates: {
+                    header: '<h3>Tags</h3>'
+                }
+            }, {
                 name: 'friends',
                 source: friends,
                 display: 'name',
@@ -37,17 +55,29 @@
                         return '<div>' + friend.name + '</div>';
                     }
                 }
-            },
-            {
-                name: 'tags',
-                source: tags,
+            }, {
+                name: 'invitable_friends',
+                source: invitable_friends,
+                display: 'name',
                 templates: {
-                    header: '<h3>Tags</h3>'
+                    suggestion: function (friend) {
+                        return '<div>' + friend.name + '</div>';
+                    }
                 }
             });
 
         $('.typeahead').bind('typeahead:select', function (ev, suggestion) {
-            if (suggestion.hasOwnProperty('name')) {
+            if (suggestion.hasOwnProperty('picture')) {
+                $('#invite-btn').click(function () {
+                    FB.ui({
+                        method: 'apprequests',
+                        to: suggestion['id'],
+                        message: 'Come and join Taaag to tag your friends!'
+                    }, function (response) {
+                        $.bootstrapGrowl('Successfully invited ' + response.to.length.toString() + ' friends.');
+                    });
+                });
+            } else if (suggestion.hasOwnProperty('name')) {
                 $(document).trigger("viewChanging", ["friend", {'id': suggestion['id']}]);
             } else {
                 $(document).trigger("viewChanging", ["tag", {'name': suggestion}]);
